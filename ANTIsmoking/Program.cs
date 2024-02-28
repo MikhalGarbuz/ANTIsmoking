143d2cc9d5b9abdc9262bbe8b97721913a71b0a9
MikhalGarbuz <garbuzmikhal@gmail.com>
2024-02-28 11:57:18 +0200

    some updates

--- ANTIsmoking/Program.cs
+++ ANTIsmoking/Program.cs
@@ -1,7 +1,110 @@
 ﻿using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+using MySql.Data.MySqlClient;
+
+var botClient = new TelegramBotClient("6436860979:AAHbR6jwJxZGtqXhbBh43VAlVTTzqoUppH8");
+
+ReceiverOptions receiverOptions = new()
+{
+    AllowedUpdates = Array.Empty<UpdateType>()
+};
+
+botClient.StartReceiving(
+    updateHandler: HandleUpdateAsync,
+    pollingErrorHandler: HandlePollingErrorAsync,
+    receiverOptions: receiverOptions
+);
+var me = await botClient.GetMeAsync();
+Console.WriteLine($"Start listening for @{me.Username}");
+Console.ReadLine();
+
+
+async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+{
+    if (update.Message is not { } message || message.Text is not { } messageText)
+        return;
+
+    var chatId = message.Chat.Id;
+
+    Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
+
+    if (message.Text is "menu")
+    {
+        InlineKeyboardMarkup inlineKeyboard = new(new[]
+        {
+            new []
+            {
+                InlineKeyboardButton.WithCallbackData(text: "back", callbackData: "locations"),
+            },
+        });
+
+        Message sentMessage = await botClient.SendTextMessageAsync(chatId,"Choose a response", replyMarkup: inlineKeyboard);
+    }
+
+    var callbackQuery = update.CallbackQuery;
+    if (callbackQuery.Data== "11")
+    {
+        Message sentMessage = await botClient.SendTextMessageAsync(chatId, "ден лох");
+
+    }
+
+    if (message.Text is "точки продажу")
+    {
+        InlineKeyboardMarkup inlineKeyboard = new(new[]
+{
+    new []
+    {
+        InlineKeyboardButton.WithCallbackData(text: "1.1", callbackData: "11"),
+        InlineKeyboardButton.WithCallbackData(text: "1.2", callbackData: "12"),
+    },
+    new []
+    {
+        InlineKeyboardButton.WithCallbackData(text: "2.1", callbackData: "21"),
+        InlineKeyboardButton.WithCallbackData(text: "2.2", callbackData: "22"),
+    },
+});
+        Message sentMessage = await botClient.SendTextMessageAsync(chatId, "Choose a response", replyMarkup: inlineKeyboard);
+    }
+
+    //if ()
+    //{
+    //    InlineKeyboardMarkup inlineKeyboard = new(new[]
+    //    {
+    //        new []
+    //        {
+    //            InlineKeyboardButton.WithCallbackData(text: "НУЛП гуртожиток 8", callbackData: "8"),
+    //            InlineKeyboardButton.WithCallbackData(text: "НУЛП гуртожиток 11", callbackData: "11"),
+    //        },
+    //        new []
+    //        {
+    //            InlineKeyboardButton.WithCallbackData(text: "НУЛП гуртожиток 14", callbackData: "14"),
+    //            InlineKeyboardButton.WithCallbackData(text: "Львів вул. Бойчука 5", callbackData: "0"),
+    //        }
+    //    });
+
+    //    Message sentMessage = await botClient.SendTextMessageAsync(chatId, "Choose a response", replyMarkup: inlineKeyboard);
+        
+    //}
+}
 
 
 
+Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+{
+    var ErrorMessage = exception switch
+    {
+        ApiRequestException apiRequestException
+            => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+        _ => exception.ToString()
+    };
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+    Console.WriteLine(ErrorMessage);
+    return Task.CompletedTask;
+}
+
+    
